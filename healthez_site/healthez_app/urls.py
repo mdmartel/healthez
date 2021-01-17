3bcad541c5d462d0c113c573b22c35e49e9ab7bd
User <mdmartel@ucdavis.edu>
2021-01-17 11:53:15 -0800

    add html linking

--- healthez_site/healthez_app/urls.py
+++ healthez_site/healthez_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.bootstrap_test,name='bootstrap_test'),
+    #path('', views.index, name='index'),
+    path('', views.bootstrap_test,name='bootstrap_test'),
     path('db_test/', views.food_test,name='food_test'),
     path('db_test_add/<str:name>', views.food_add,name='food_add'),
     path('input/', views.search_test,name='search_test'),
