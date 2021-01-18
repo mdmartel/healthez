d52b62692f158b3d213631a843e8c5c405e2f998
mdmartel <47866900+mdmartel@users.noreply.github.com>
2021-01-17 17:54:59 -0800

    Update settings.py

--- healthez_site/healthez_site/settings.py
+++ healthez_site/healthez_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '($6fqi!rwv*lkq_o_46(blac==8u!d%hb7px*iq!j1^s72g3p8'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
